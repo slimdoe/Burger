0f573acf0b4a616a70fe1eeb423418613020ec24
Sy Lim <slimdoe2@gmail.com>
2019-04-02 18:37:52 -0700

    added all necessary folder and files

---
+++ DB/schema.sql
@@ -0,0 +1,9 @@
+-- Write SQL queries this file that do the following:
+
+--    * Create the `burgers_db`.
+--    * Switch to or use the `burgers_db`.
+--    * Create a `burgers` table with these fields:
+--      * **id**: an auto incrementing int that serves as the primary key.
+--      * **burger_name**: a string.
+--      * **devoured**: a boolean.
+
---
+++ DB/seeds.sql
@@ -0,0 +1,1 @@
+-- write insert queries to populate the `burgers` table with at least three entries.
